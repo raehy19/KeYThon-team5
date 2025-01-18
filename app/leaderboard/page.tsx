525f0a4ff80e63c0624ba8caa95b8f4c06e4d493
Raehyeon Jeong <raehy19@gmail.com>
2025-01-18 20:53:00 +0900

    feat : remove tutorials, add basic game page

---
+++ app/leaderboard/page.tsx
@@ -0,0 +1,3 @@
+export default async function LeaderBoard() {
+  return <div className='flex-1 w-full flex flex-col gap-12'></div>;
+}
--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -1,7 +1,7 @@
-import FetchDataSteps from "@/components/tutorial/fetch-data-steps";
-import { createClient } from "@/utils/supabase/server";
-import { InfoIcon } from "lucide-react";
-import { redirect } from "next/navigation";
+import { createClient } from '@/utils/supabase/server';
+import { redirect } from 'next/navigation';
+import Link from 'next/link';
+import { Button } from '@/components/ui/button';
 
 export default async function ProtectedPage() {
   const supabase = await createClient();
@@ -11,27 +11,22 @@ export default async function ProtectedPage() {
   } = await supabase.auth.getUser();
 
   if (!user) {
-    return redirect("/sign-in");
+    return redirect('/sign-in');
   }
 
   return (
-    <div className="flex-1 w-full flex flex-col gap-12">
-      <div className="w-full">
-        <div className="bg-accent text-sm p-3 px-5 rounded-md text-foreground flex gap-3 items-center">
-          <InfoIcon size="16" strokeWidth={2} />
-          This is a protected page that you can only see as an authenticated
-          user
-        </div>
-      </div>
-      <div className="flex flex-col gap-2 items-start">
-        <h2 className="font-bold text-2xl mb-4">Your user details</h2>
-        <pre className="text-xs font-mono p-3 rounded border max-h-32 overflow-auto">
-          {JSON.stringify(user, null, 2)}
-        </pre>
-      </div>
-      <div>
-        <h2 className="font-bold text-2xl mb-4">Next steps</h2>
-        <FetchDataSteps />
+    <div className='flex-1 w-full flex flex-col gap-12'>
+      <div className='flex flex-col gap-2 items-center justify-center'>
+        <Button>
+          <Link href='/leaderboard' className='text-accent font-bold'>
+            Leaderboard
+          </Link>
+        </Button>
+        <Button>
+          <Link href='/game' className='text-accent font-bold'>
+            Play the game
+          </Link>
+        </Button>
       </div>
     </div>
   );
